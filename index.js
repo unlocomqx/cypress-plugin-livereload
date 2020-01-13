8faa2ed8f9b03fd4c26912c005efaf6eb20827eb
unlocomqx <amxmodx.unloco@gmail.com>
2020-01-13 21:36:43 +0100

    support socket options

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-const debounce = require('debounce');
-const ws = new WebSocket("wss://dev.net:35730/livereload");
+const {join} = require("path");
+const debounce = require("debounce");
 
-let reloadCypress = () => {
-  console.info("Cypress reload triggered by livereload");
-  window.top.document.querySelector(".reporter .restart").click();
+const getUrl = (options) => {
+  const protocol = options.https ? "wss" : "ws";
+  return `${protocol}://${options.hostname}:${options.port}/livereload`;
 };
-const debouncedReload = debounce(reloadCypress, 333);
 
-beforeEach(async () => {
-  ws.onmessage = (ev) => {
-    if (ev.type === "message" && ev.data) {
-      try {
-        const data = JSON.parse(ev.data);
-        if (data.command === "reload") {
-          debouncedReload();
+module.exports = function (options) {
+
+  options = Object.assign({
+    hostname: null,
+    https: false,
+    port: 35729,
+    delay: 0,
+  }, options);
+
+  if (!options.hostname) {
+    options.hostname = window.location.hostname;
+  }
+
+  const ws = new WebSocket(getUrl(options));
+
+  if (ws) {
+    let reloadCypress = () => {
+      setTimeout(() => {
+        console.info("Cypress reload triggered by livereload");
+        window.top.document.querySelector(".reporter .restart").click();
+      }, options.delay);
+    };
+    const debouncedReload = debounce(reloadCypress, 333);
+
+    beforeEach(async () => {
+      ws.onmessage = (ev) => {
+        if (ev.type === "message" && ev.data) {
+          try {
+            const data = JSON.parse(ev.data);
+            if (data.command === "reload") {
+              debouncedReload();
+            }
+          } catch (e) {
+            console.error("Could not parse message from livereload");
+            console.error(e.message);
+            console.error("Original data");
+            console.error(ev.data);
+          }
         }
-      } catch (e) {
-        console.error("Could not parse message from livereload");
-        console.error(e.message);
-        console.error("Original data");
-        console.error(ev.data);
-      }
-    }
-  };
-});
+      };
+    });
+  }
+};
