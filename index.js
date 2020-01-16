d7223b372e9b930f17bcde83bf2e361e35a51c22
unlocomqx <amxmodx.unloco@gmail.com>
2020-01-16 18:23:28 +0100

    remove unused variable

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const {join} = require("path");
 const debounce = require("debounce");
 
 const config = Cypress.config("livereload") || {};
@@ -20,31 +19,30 @@ if (!options.hostname) {
 }
 
 const ws = new WebSocket(getUrl(options));
+console.log(ws);
+let reloadCypress = () => {
+  setTimeout(() => {
+    console.info("Cypress reload triggered by livereload");
+    window.top.document.querySelector(".reporter .restart").click();
+  }, options.delay);
+};
+const debouncedReload = debounce(reloadCypress, 333);
 
-if (ws) {
-  let reloadCypress = () => {
-    setTimeout(() => {
-      console.info("Cypress reload triggered by livereload");
-      window.top.document.querySelector(".reporter .restart").click();
-    }, options.delay);
-  };
-  const debouncedReload = debounce(reloadCypress, 333);
-
-  beforeEach(async () => {
-    ws.onmessage = (ev) => {
-      if (ev.type === "message" && ev.data) {
-        try {
-          const data = JSON.parse(ev.data);
-          if (data.command === "reload") {
-            debouncedReload();
-          }
-        } catch (e) {
-          console.error("Could not parse message from livereload");
-          console.error(e.message);
-          console.error("Original data");
-          console.error(ev.data);
+beforeEach(() => {
+  ws.onmessage = (ev) => {
+    console.log(ev.type);
+    if (ev.type === "message" && ev.data) {
+      try {
+        const data = JSON.parse(ev.data);
+        if (data.command === "reload") {
+          debouncedReload();
         }
+      } catch (e) {
+        console.error("Could not parse message from livereload");
+        console.error(e.message);
+        console.error("Original data");
+        console.error(ev.data);
       }
-    };
-  });
-}
+    }
+  };
+});
